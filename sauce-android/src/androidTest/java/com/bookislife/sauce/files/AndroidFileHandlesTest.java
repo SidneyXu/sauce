00ca8f5095acd23479ad7763566ba9df7481c072
sidneyy <siriuseddy@gmail.com>
2015-09-14 17:35:45 +0800

    Update

---
+++ sauce-android/src/androidTest/java/com/bookislife/sauce/files/AndroidFileHandlesTest.java
@@ -0,0 +1,45 @@
+package com.bookislife.sauce.files;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by mrseasons on 2015/09/14.
+ */
+public class AndroidFileHandlesTest extends TestCase {
+
+    public void testAbsolute() throws Exception {
+
+    }
+
+    public void testAbsolute1() throws Exception {
+
+    }
+
+    public void testAbsolute2() throws Exception {
+
+    }
+
+    public void testAbsolute3() throws Exception {
+
+    }
+
+    public void testInternal() throws Exception {
+
+    }
+
+    public void testExternal() throws Exception {
+
+    }
+
+    public void testSdcard() throws Exception {
+
+    }
+
+    public void testAssets() throws Exception {
+
+    }
+
+    public void testInternal1() throws Exception {
+
+    }
+}
